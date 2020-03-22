10bffcdae8eef169667fbf2b707f013130ecc41c
Zoltan Herczeg <zoltan.herczeg>
2020-03-22 22:08:59 +0100

    LoopThrough specific folder and list folders and files

--- FileOrganizer/Program.cs
+++ FileOrganizer/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,31 @@ namespace FileOrganizer
     {
         static void Main(string[] args)
         {
+            LoopThrough();
+        }
+
+        static void LoopThrough()
+        {
+            List<string> filesList = Directory.GetFiles(@"C:\Users\Herczeg Zoltán\Desktop\Test").ToList();
+            List<string> folderList = Directory.GetDirectories(@"C:\Users\Herczeg Zoltán\Desktop\Test").ToList();
+
+
+            foreach (var item in filesList)
+            {
+                var extension = Path.GetExtension(item);
+                if (true)
+                {
+
+                }
+                Console.WriteLine("ext:" + extension + " " + item);
+            }
+
+            foreach (var item in folderList)
+            {
+                string getFolderName = new DirectoryInfo(Path.GetDirectoryName(item)).Name;
+
+                Console.WriteLine("folder:" + getFolderName + " " + item);
+            }
         }
     }
 }
