90d713f5a55ce8fee30ef5b4bb23f1ab142c99b5
Zoltan Herczeg <zoltan.herczeg>
2020-03-22 23:27:11 +0100

    Adding folder watcher.

--- FileOrganizer/Program.cs
+++ FileOrganizer/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Security.Permissions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,30 +10,54 @@ namespace FileOrganizer
 {
     class Program
     {
+        
         static void Main(string[] args)
         {
-            LoopThrough();
+            string path = @"C:\Users\Herczeg Zoltán\Desktop\Test";
+            
+            
+
+            Run(path);
         }
 
-        static void LoopThrough()
+        [PermissionSet(SecurityAction.Demand, Name ="FullTrust")]
+        private static void Run(string path)
         {
-            List<string> filesList = Directory.GetFiles(@"C:\Users\Herczeg Zoltán\Desktop\Test").ToList();
-            List<string> folderList = Directory.GetDirectories(@"C:\Users\Herczeg Zoltán\Desktop\Test").ToList();
+            using (FileSystemWatcher watcher = new FileSystemWatcher())
+            {
+
+                watcher.Path = path;
+
+                watcher.Changed += OnChanged;
+                
+                watcher.NotifyFilter = NotifyFilters.Size;
+                watcher.EnableRaisingEvents = true;
+
+                // Wait for the user to quit the program.
+                Console.WriteLine("Press 'q' to quit .");
+
+                while (Console.Read() != 'q') ;
+            } 
+        }
+
+        private static void OnChanged(object source, FileSystemEventArgs e)
+        {
+            LoopThrough(e.FullPath);
+        }
+
+        static void LoopThrough(string path)
+        {
+            path = Path.GetDirectoryName(path);
+
+            List<string> filesList = Directory.GetFiles(path).ToList();
+            List<string> folderList = Directory.GetDirectories(path).ToList();
 
             foreach (var item in filesList)
             {
                 GetFolderNameOfFile(item);
                 MoveFile(item);
             }
-
-            //foreach (var item in folderList)
-            //{
-            //    string getFolderName = new DirectoryInfo(Path.GetDirectoryName(item)).Name;
-
-            //    Console.WriteLine("folder:" + getFolderName + " " + item);
-            //}
         }
-
 
         static bool GetFolderNameOfFile(string item)
         {
@@ -51,8 +76,21 @@ namespace FileOrganizer
             var extension = Path.GetExtension(filePath);
             var name = Path.GetFileName(filePath);
             var filePathWithoutFileName = Path.GetDirectoryName(filePath);
-
-            File.Move(filePath,Path.Combine(filePathWithoutFileName, extension,name));
+            try
+            {
+                File.Move(filePath, Path.Combine(filePathWithoutFileName, extension, name));
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Could not move the file yet probably because it was large and still copying into the folder!");                try
+                {
+                    MoveFile(filePath);
+                    Console.WriteLine("It could move the file!");
+                }
+                catch (Exception)
+                {
+                }
+            }
         }
     }
 }
